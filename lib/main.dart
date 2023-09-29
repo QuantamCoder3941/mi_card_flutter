a754db26a0174aaa69c003028f45377ea9a86c62
QuantamCoder3941 <=>
2023-09-29 21:48:55 +0500

    added asset image

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,82 @@ void main() {
   );
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.teal,
+//         body: SafeArea(
+//           child: Row(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+//             // these are some properties of column
+//             // crossAxisAlignment: CrossAxisAlignment.start,
+//             // crossAxisAlignment: CrossAxisAlignment.end,
+//             // mainAxisAlignment: MainAxisAlignment.center,
+//             // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             // verticalDirection: VerticalDirection.down,
+//             // mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Container(
+//                 width: 100.0,
+
+//                 // margin: EdgeInsets.all(30.0)
+//                 // margin: EdgeInsets.fromLTRB(30.0, 10.0, 40.0, 20.0),
+//                 // margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+//                 //// padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+//                 color: Colors.red,
+//                 child: Text("Container 1"),
+//               ),
+
+//               // the sized box can be used to give space between conatiners
+//               // SizedBox(
+//               //   height: 10.0,
+//               // ),
+
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Container(
+//                     height: 100.0,
+//                     width: 100.0,
+//                     color: Colors.yellow,
+//                   ),
+//                   Container(
+//                     height: 100.0,
+//                     width: 100.0,
+//                     color: Colors.tealAccent,
+//                   )
+//                 ],
+//               ),
+
+//               // SizedBox(
+//               //   width: 10.0,
+//               // ),
+
+//               Container(
+//                 height: 100.0,
+//                 color: Colors.blue,
+//                 child: Text("Container 3"),
+//               ),
+
+//               // the width can be set to infinity to get streched containers
+//               // Container(
+//               //   width: double.infinity,
+//               // )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -14,66 +90,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        // appBar: AppBar(
+        //   title: Text("MyApp"),
+        // ),
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-            // these are some properties of column
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // verticalDirection: VerticalDirection.down,
-            // mainAxisSize: MainAxisSize.min,
+          child: Column(
             children: [
-              Container(
-                width: 100.0,
-
-                // margin: EdgeInsets.all(30.0)
-                // margin: EdgeInsets.fromLTRB(30.0, 10.0, 40.0, 20.0),
-                // margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
-                //// padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                color: Colors.red,
-                child: Text("Container 1"),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("Images/bilal.jpg"),
               ),
-
-              // the sized box can be used to give space between conatiners
-              // SizedBox(
-              //   height: 10.0,
-              // ),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.tealAccent,
-                  )
-                ],
-              ),
-
-              // SizedBox(
-              //   width: 10.0,
-              // ),
-
-              Container(
-                height: 100.0,
-                color: Colors.blue,
-                child: Text("Container 3"),
-              ),
-
-              // the width can be set to infinity to get streched containers
-              // Container(
-              //   width: double.infinity,
-              // )
             ],
           ),
         ),

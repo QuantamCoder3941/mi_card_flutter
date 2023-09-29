77a4607540b1054a98c394745dd0459a93d5df3d
QuantamCoder3941 <=>
2023-09-29 21:33:53 +0500

    Column and row with challenge

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            // these are some properties of column
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            // crossAxisAlignment: CrossAxisAlignment.end,
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            // verticalDirection: VerticalDirection.down,
+            // mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 100.0,
+
+                // margin: EdgeInsets.all(30.0)
+                // margin: EdgeInsets.fromLTRB(30.0, 10.0, 40.0, 20.0),
+                // margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+                //// padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                color: Colors.red,
+                child: Text("Container 1"),
+              ),
+
+              // the sized box can be used to give space between conatiners
+              // SizedBox(
+              //   height: 10.0,
+              // ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.tealAccent,
+                  )
+                ],
+              ),
+
+              // SizedBox(
+              //   width: 10.0,
+              // ),
+
+              Container(
+                height: 100.0,
+                color: Colors.blue,
+                child: Text("Container 3"),
+              ),
+
+              // the width can be set to infinity to get streched containers
+              // Container(
+              //   width: double.infinity,
+              // )
+            ],
+          ),
+        ),
       ),
     );
   }
